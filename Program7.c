7cea14a0fa6023203ed77fed3c9082be0c400613
PoornikaPant <pantpoornika@gmail.com>
2026-08-14 09:56:39 +0530

    Add files via upload

---
+++ Program7.c
@@ -0,0 +1,20 @@
+//Write a program to swap two numbers without using a third variable.
+
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    printf("Before swapping: num1 = %d, num2 = %d\n", num1, num2);
+
+    num1 = num1 + num2;
+    num2 = num1 - num2;
+    num1 = num1 - num2;
+
+    printf("After swapping: num1 = %d, num2 = %d\n", num1, num2);
+
+    return 0;
+}
