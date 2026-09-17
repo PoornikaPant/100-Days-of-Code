f298bcfee81d603bf5cb20117191f2df52bbfe95
PoornikaPant <pantpoornika@gmail.com>
2026-09-18 03:30:32 +0530

    Add files via upload

---
+++ Program79.c
@@ -0,0 +1,43 @@
+//Perform diagonal traversal of a matrix.
+#include <stdio.h>
+
+int main()
+{
+    int a[10][10];
+    int rows, cols;
+    int i, j, d;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+
+    printf("Enter the elements of the matrix:\n");
+
+    for(i = 0; i < rows; i++)
+    {
+        for(j = 0; j < cols; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("Diagonal traversal:\n");
+
+    // Traverse diagonals
+    for(d = 0; d < rows + cols - 1; d++)
+    {
+        for(i = 0; i < rows; i++)
+        {
+            j = d - i;
+
+            if(j >= 0 && j < cols)
+            {
+                printf("%d ", a[i][j]);
+            }
+        }
+    }
+
+    return 0;
+}
